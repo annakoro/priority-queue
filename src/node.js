1041b3eb56a28e46ac148dee9625858e90c447fd
annakoro <ann.korostelev@gmail.com>
2017-11-08 11:53:34 +0300

    Update node.js

--- src/node.js
+++ src/node.js
@@ -2,25 +2,44 @@ class Node {
 	constructor(data, priority) {
 		this.data = data;
 		this.priority = priority;
-		this.parent=null; 
-		this.left= null;
-		this.right= null;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		if (!this.left) {
+			this.left = node;
+		} else if (!this.right) {
+			this.right = node;
+		}
+		node.parent = this;
 	}
 
 	removeChild(node) {
-
+		if (this.left == node) {
+			this.left = null;
+			node.parent = null;
+		} else
+			if (this.right == node) {
+				this.right = null;
+				node.parent = null;
+			} else throw new Error("passed node is not a child of this node");
 	}
 
 	remove() {
-
+		if (this.parent) {
+			this.parent.removeChild(this);
+		}
 	}
 
 	swapWithParent() {
-		
+		if (this.parent) {
+			// var temp;
+			// this.temp = this.parent;
+			// this.parent = this;
+			// this = this.temp;
+		}
 	}
 }
 
