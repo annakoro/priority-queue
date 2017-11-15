11bbecac17c8676b85ca56b07af9215725841ad4
annakoro <ann.korostelev@gmail.com>
2017-11-15 14:56:14 +0300

    Update max-heap.js

--- src/max-heap.js
+++ src/max-heap.js
@@ -7,43 +7,46 @@ class MaxHeap {
 	}
 
 	push(data, priority) {
-		
+		this.node = new Node(data, priority);
+		this.insertNode(this.node);
+		this.shiftNodeUp(this.node);
 	}
 
 	pop() {
-		
+
 	}
 
 	detachRoot() {
-		
+
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		
+
 	}
 
 	size() {
-		
+
 	}
 
 	isEmpty() {
-		
+		return this.root === null;
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	insertNode(node) {
-		
+		if(this.isEmpty) this.root = node;
 	}
 
 	shiftNodeUp(node) {
-		
+
 	}
 
 	shiftNodeDown(node) {
-		
+
 	}
 }
 
