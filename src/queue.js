52781e43bfe8890ebdff5448317ba59f338aacf7
annakoro <ann.korostelev@gmail.com>
2017-11-26 14:34:21 +0300

    Update queue.js

--- src/queue.js
+++ src/queue.js
@@ -2,13 +2,12 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-		if (maxSize) {
+		if (maxSize){
 			this.maxSize = maxSize;
 		} else {
 			this.maxSize = 30;
 		}
 		this.heap = new MaxHeap();
-
 	}
 
 	push(data, priority) {
