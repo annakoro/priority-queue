fd358a15b7386a8cea143ab5de6a5305c975d028
annakoro <ann.korostelev@gmail.com>
2017-11-26 14:40:15 +0300

    Update node.js

--- src/node.js
+++ src/node.js
@@ -2,29 +2,33 @@ class Node {
 	constructor(data, priority) {
 		this.data = data;
 		this.priority = priority;
-		this.parent = null;
 		this.left = null;
 		this.right = null;
+		this.parent = null;
 	}
 
 	appendChild(node) {
 		if (!this.left) {
 			this.left = node;
-		} else if (!this.right) {
+		}
+		else if (!this.right) {
 			this.right = node;
 		}
-		node.parent = this;
+		if (node) {
+			node.parent = this;
+		}
 	}
 
 	removeChild(node) {
-		if (this.left == node) {
-			this.left = null;
+		if (node === this.left) {
 			node.parent = null;
-		} else
-			if (this.right == node) {
-				this.right = null;
-				node.parent = null;
-			} else throw new Error("passed node is not a child of this node");
+			this.left = null;
+		}
+		else if (node === this.right) {
+			node.parent = null;
+			this.right = null;
+		}
+		else throw new Error('This parent do not have the child');
 	}
 
 	remove() {
@@ -33,42 +37,42 @@ class Node {
 		}
 	}
 
-		swapWithParent() {
-			if (this.parent) {
-				var granddad = this.parent.parent;
-				var dad = this.parent;
-				var node;
-				var leftSon = this.left;
-				var rightSon = this.right;
-	
-				if (granddad) {
-					if (this.parent === granddad.left) {
-						granddad.left = this;
-					} else if (this.parent === granddad.right) {
-						granddad.right = this;
-					}
+	swapWithParent() {
+		if (this.parent) {
+			var ded = this.parent.parent;
+			var papa = this.parent;
+			var bro;
+			var leftSon = this.left;
+			var rightSon = this.right;
+
+			if (ded) {
+				if (this.parent === ded.left) {
+					ded.left = this;
+				} else if (this.parent === ded.right) {
+					ded.right = this;
 				}
-	
-				if (this === dad.left){
-					node = this.parent.right;
-					this.right = node;
-					this.left = dad;
-				} else if (this === dad.right){
-					node = this.parent.left;
-					this.left = node;
-					this.right = dad;
-				}
-	
-				this.parent = granddad;
-	
-				if (node) { node.parent = this };
-				dad.parent = this;
-				dad.left = leftSon;
-				dad.right = rightSon;
-				if (leftSon) { leftSon.parent = dad };
-				if (rightSon) { rightSon.parent = dad };
 			}
+
+			if (this === papa.left){
+				bro = this.parent.right;
+				this.right = bro;
+				this.left = papa;
+			} else if (this === papa.right){
+				bro = this.parent.left;
+				this.left = bro;
+				this.right = papa;
+			}
+
+			this.parent = ded;
+
+			if (bro) { bro.parent = this };
+			papa.parent = this;
+			papa.left = leftSon;
+			papa.right = rightSon;
+			if (leftSon) { leftSon.parent = papa };
+			if (rightSon) { rightSon.parent = papa };
 		}
 	}
+}
 
 module.exports = Node;
