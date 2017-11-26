d3990363cf441bdff399599f614d86b64963abe3
annakoro <ann.korostelev@gmail.com>
2017-11-26 14:30:19 +0300

    Update queue.js

--- src/queue.js
+++ src/queue.js
@@ -12,19 +12,27 @@ class PriorityQueue {
 	}
 
 	push(data, priority) {
-		this.heap.push(data, priority);
+		if (this.size() < this.maxSize){
+			this.heap.push(data, priority);
+		} else {
+			throw new Error('Queue is over');
+		}
 	}
 
 	shift() {
-
+		if (this.size() > 0) {
+			return this.heap.pop();
+		} else {
+			throw new Error('Queue is empty');
+		}
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-
+		return this.heap.isEmpty();
 	}
 }
 
